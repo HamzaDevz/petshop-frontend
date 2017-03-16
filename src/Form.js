46218273b9e8a18174c5acdba0ec13035250d33d
hamzatei <ht@tradelab.fr>
2017-03-16 18:18:34 +0100

    Add bootstrap, react friendly for form.

--- src/Form.js
+++ src/Form.js
@@ -4,93 +4,83 @@ import config from './config';
 import _ from 'lodash';
 import './Form.css';
 
-// MyElems
-const ButtonDelete = (props) => !props.show ? null : <button className="btn" onClick={props.onDelete}>Close Form</button>;
-const ButtonAdd = (props) => <button className="btn" onClick={props.onAdd}>Add new pets</button>;
-const ErrorForm = () => <p>mon message d'erreur</p>;
-const Error = (props) => !props.show ? null : <ErrorForm />;
+const ToggleBtn = (props) => <button className="btn btn-primary" onClick={props.onClick}>{props.formState ? 'Close form' : 'Add a Pet'}</button>;
+const Message = (props) => !props.message ? null : <p className="bg-danger">{props.message}</p>;
 
 const Forms = (props) =>
   (props.showForm && props.onSubmit) ?
-    (<form onSubmit={props.onSubmit}>
-      <label>
-        Type:
-        <input type="text" name="type" />
-      </label>
-      <label>
-        Name:
-        <input type="text" name="name" />
-      </label>
-      <label>
-        Age:
-        <input type="text" name="age" />
-      </label>
-      <label>
-        Description:
-        <input type="text" name="description" />
-      </label>
-      <input type="submit" value="Add me" />
-    </form>) :
-    null;
+    (<form className="form-inline" onSubmit={props.onSubmit}>
+        <div className="form-group">
+          <label htmlFor="type">Type</label>
+          <input type="text" className="form-control" name="type" placeholder="Mammal" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input type="text" className="form-control" name="name" placeholder="Horse" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="age">Age</label>
+          <input type="text" name="age" className="form-control" placeholder="17" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="description">Description</label>
+          <textarea className="form-control" name="description" placeholder="Horse's description" rows="3" />
+        </div>
+        <button type="submit" className="btn btn-default">Add me</button>
+      </form>)
+    : null;
 
 class Form extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      showError: false,
-      displayForm: false,
-      showForm: false
+      showMessage: false,
+      showForm: false,
+      message: ''
     };
 
     this.config = new config;
-    this.addForm = this.addForm.bind(this);
-    this.removeForm = this.removeForm.bind(this);
+    this.handleToggleClick = this.handleToggleClick.bind(this);
     this.launchApi = this.launchApi.bind(this);
   }
 
-  clearAll() {
-    return {showForm: false, showError: false};
-  }
-
-  addForm() {
-    this.setState({showForm: true, showError: false});
-  }
-
-  removeForm() {
-    this.setState(this.clearAll());
+  handleToggleClick() {
+    this.setState(prevState => ({showForm: !prevState.showForm, message: ''}));
   }
 
   launchApi = (e) => {
     e.preventDefault();
 
-    let array = {type: 'required', name: 'required', age: 'optional', description: 'optional'};
+    let rules = {type: 'required', name: 'required', age: 'optional', description: 'optional'};
     let values = {};
     let isValid = true;
 
-    _.forEach(array, (v, k) => (v === 'required' && _.isEmpty(e.target[k].value)) ? isValid = false : values[k] = e.target[k].value);
+    _.forEach(rules, (v, k) => (v === 'required' && _.isEmpty(e.target[k].value)) ? isValid = false : values[k] = e.target[k].value);
 
-    this.setState({showError: !isValid});
+    if (!isValid) {
+      this.setState({message: 'You need to fill all required fields'});
+    }
 
     if (isValid) {
       axios.post(this.config.myHost() + this.config.myEndpoint('pets'), values)
         .then(function (data) {
-          // OK ! reload table pets
-        })
+          // OK !
+          this.setState({message: 'Pet registrered', pets: data});
+        }.bind(this))
         .catch(function (error) {
-          // ERROR display !
-          console.log('request failed', error);
-        });
+          // ERROR !
+          this.setState({message: error});
+        }.bind(this));
     }
   };
 
   render() {
     return (
-      <div>
-        <ButtonAdd class="btn" onAdd={this.addForm} />
-        <Forms showForm={this.state.showForm} onSubmit={this.launchApi} />
-        <ButtonDelete show={this.state.showForm} class="btn" onDelete={this.removeForm} />
-        <Error show={this.state.showError} />
+      <div className="container-form">
+        <ToggleBtn class="btn" formState={this.state.showForm} onClick={this.handleToggleClick} />
+        <Forms showForm={this.state.showForm} pets={this.state.pets} onSubmit={this.launchApi} />
+        <Message message={this.state.message} />
       </div>
     );
   }
